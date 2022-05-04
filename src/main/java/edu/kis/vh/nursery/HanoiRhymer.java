36455ec5bcd246624b683f25642cfda48866b4d7
Jakub_Bernaciak_235830 <235830@edu.p.lodz.pl>
2022-05-04 13:32:13 +0200

    accessory i hermetyzacja

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,17 +2,26 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-	int totalRejected = 0;
+	public int getTotalRejected() {
+		return totalRejected;
+	}
+
+	private int totalRejected = 0;
+
 
 	public int reportRejected() {
-		return totalRejected;
+		return getTotalRejected();
 	}
 
 	public void countIn(int in) {
 		if (!callCheck() && in > peekaboo())
-			totalRejected++;
+			setTotalRejected(getTotalRejected() + 1);
 		else
 			super.countIn(in);
+	}
+
+	public void setTotalRejected(int totalRejected) {
+		this.totalRejected = totalRejected;
 	}
 	//ctrl + <- i ctrl + -> przełącza nas między plikami w edytorze
 	//ctrl +alt + l reformatuje nam aktualny kod
