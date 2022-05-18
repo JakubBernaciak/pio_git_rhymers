3c13d643374b942f7e681508421044bfe3f28b89
JakubBernaciak <235830@edu.p.lodz.pl>
2022-05-18 11:05:03 +0200

    Dodanie szkieletu dokumentacji do Node

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,7 +1,13 @@
 package edu.kis.vh.nursery.list;
 
 public class Node {
-
+    /**
+     * protected Node prev - poprzedni Node listy
+     * protected Node next - następny Node listy
+     * private final int value - wartość, którą przechowuje dany Node
+     * public Node(int i) - kontruktor Noda, który odrazu go inicjalizuje
+     * public int getValue() - zwraca wartość, którą przechoowuje Node
+     */
     final private int value;
     protected Node prev, next;
 
